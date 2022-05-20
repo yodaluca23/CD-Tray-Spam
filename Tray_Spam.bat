5fef240897516274c67ed9ee7268fc444d8daaee
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2022-05-20 13:06:49 -0500

    Update Tray_Spam.bat

--- Tray_Spam.bat
+++ Tray_Spam.bat
@@ -10,4 +10,19 @@ echo oWMP.close                                 >> %temp%\temp.vbs
 %temp%\temp.vbs
 timeout /t 1
 del %temp%\temp.vbs
+
+Set oWMP = CreateObject("WMPlayer.OCX.7" )  >> %temp%\temp.vbs
+Set colCDROMs = oWMP.cdromCollection  >> %temp%\temp.vbs
+if colCDROMs.Count >= 1 then  >> %temp%\temp.vbs
+  >> %temp%\temp.vbs
+For i = 0 to colCDROMs.Count - 1  >> %temp%\temp.vbs
+colCDROMs.Item(i).Eject  >> %temp%\temp.vbs
+Next ' cdrom  >> %temp%\temp.vbs
+  >> %temp%\temp.vbs
+For i = 0 to colCDROMs.Count - 1  >> %temp%\temp.vbs
+colCDROMs.Item(i).Eject  >> %temp%\temp.vbs
+Next ' cdrom  >> %temp%\temp.vbs
+  >> %temp%\temp.vbs
+End If  >> %temp%\temp.vbs
+
 goto Task
